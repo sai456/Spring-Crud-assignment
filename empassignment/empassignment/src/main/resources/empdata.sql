dc1d22cd628f27b858bf21e4450b097b5a67556f
sai456 <91714383+sai456@users.noreply.github.com>
2022-03-04 12:07:25 +0530

    Add files via upload

---
+++ empassignment/empassignment/src/main/resources/empdata.sql
@@ -0,0 +1,25 @@
+insert into employee(id, first_name, last_name) 
+values(101,'sai','ram');
+insert into employee (id, first_name, last_name) 
+values(102,'balaji' ,'kalla' );
+insert into employee(id, first_name, last_name ) 
+values(103,'purna', 'satish');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
